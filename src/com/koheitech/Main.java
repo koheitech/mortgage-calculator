525c6f0b19d2bb1a75e912ed023f0085af977426
koheitech <koheitechigh@gmail.com>
2022-11-13 10:24:11 +0200

    add input validation

--- src/com/koheitech/Main.java
+++ src/com/koheitech/Main.java
@@ -11,15 +11,31 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principle($): ");
-        int principle = scanner.nextInt();
+        int principle;
+        while(true) {
+            System.out.print("Principle($1K - $1M): ");
+            principle = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate(%): ");
-        float annualInterest = scanner.nextFloat();
+            if (1_000 <= principle && principle <= 1_000_000) break;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
+
+        float annualInterest;
+        while(true) {
+            System.out.print("Annual Interest Rate(%): ");
+            annualInterest = scanner.nextFloat();
+            if (0 < annualInterest && annualInterest <= 100) break;
+            System.out.println("Enter a value greater than 0 or value less or equal to 100");
+        }
         float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
 
-        System.out.print("Period (Year): ");
-        int year = scanner.nextInt();
+        int year;
+        while (true){
+            System.out.print("Period (Year): ");
+            year = scanner.nextInt();
+            if (1 < year && year <= 30) break;
+            System.out.println("Enter a number between 1 and 30");
+        }
         int numberOfPayments = year * MONTH_IN_YEAR;
 
         double mortgage =
