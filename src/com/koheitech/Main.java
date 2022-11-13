f8238a4dc1c5212a7a6deb406eb71fb610801f1f
koheitech <koheitechigh@gmail.com>
2022-11-13 11:15:05 +0200

    add remaining balance calculator

--- src/com/koheitech/Main.java
+++ src/com/koheitech/Main.java
@@ -5,17 +5,32 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTH_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
 
-        double principle = readNumber("Principle($1K - $1M): ", 1_000, 1_000_000);
+        double principal = readNumber("Principle($1K - $1M): ", 1_000, 1_000_000);
         double annualInterest = readNumber("Annual Interest Rate(%): ", 0.01, 100);
         double year = readNumber("Period (Year): ", 1, 30);
 
+        printMortgage(principal, annualInterest, year);
+        printPaymentSchedule(principal, annualInterest, year);
+    }
 
-        double mortgage = calculateMortgage(principle, annualInterest, year);
+    public static void printPaymentSchedule(double principal, double annualInterest, double year) {
+        System.out.println("---------- PAYMENT SCHEDULE ----------");
+        for (short month = 1; month <= year * MONTH_IN_YEAR; ++month) {
+            double balance = calculateBalance(principal, annualInterest, year, month);
+            String balanceFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(balance);
+            System.out.println(balanceFormatted);
+        }
+    }
 
+    public static void printMortgage(double principal, double annualInterest, double year) {
+        double mortgage = calculateMortgage(principal, annualInterest, year);
+        System.out.println("---------- MORTGAGE ----------");
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
-        System.out.println("Mortgage per month: " + mortgageFormatted);
+        System.out.println("Monthly Payments: " + mortgageFormatted);
     }
 
     public static double readNumber(String prompt, double min, double max) {
@@ -30,15 +45,21 @@ public class Main {
         return input;
     }
 
-    public static double calculateMortgage(double principle, double annualInterest, double year) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
+    public static double calculateBalance(double principal, double annualInterest, double year, short numberOfPaymentsMade) {
         double monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
         double numberOfPayments = year * MONTH_IN_YEAR;
 
-        return principle *
-                ( monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) /
-                (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        return principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    }
+
+    public static double calculateMortgage(double principal, double annualInterest, double year) {
+        double monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+        double numberOfPayments = year * MONTH_IN_YEAR;
+
+        return principal
+                * ( monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
     }
 }
